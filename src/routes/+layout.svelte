3cd43d7e1733b103aee2870113dc2330e04cbef3
rohinsood <rs.rohinsood@gmail.com>
2025-03-07 10:02:02 -0800

    boilerplate

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  let { children } = $props();
+  import "../app.css";
+</script>
+{@render children()}
+
+
+<style lang="postcss">
+  :global(html) {
+    /* background-color: theme(colors.slate.50); */
+    overflow: hidden;
+  }
+</style>
